629572b66770d71b7bd79f9d010471cd59cea407
Antoine <130611615+anonymousgrasshopper@users.noreply.github.com>
2025-08-12 17:25:49 +0200

    Update log.hpp

--- include/oly/log.hpp
+++ include/oly/log.hpp
@@ -4,9 +4,17 @@
 
 enum class severity;
 
+inline bool operator<(severity a, severity b);
+inline bool operator>(severity a, severity b);
+inline bool operator<=(severity a, severity b);
+inline bool operator>=(severity a, severity b);
+
 enum class logopt : unsigned;
 
 inline logopt operator|(logopt a, logopt b);
 inline logopt& operator|=(logopt& a, logopt b);
 
+namespace Log {
+inline severity log_level;
 void Log(severity level, const std::string& message, logopt opts = logopt::NONE, std::string cmd = "");
+}
