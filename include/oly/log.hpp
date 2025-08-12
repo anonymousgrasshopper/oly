95c423eb1ef883b0d2f76c22d343e213a9482e5a
Antoine <130611615+anonymousgrasshopper@users.noreply.github.com>
2025-08-12 17:01:44 +0200

    Update log.hpp

--- include/oly/log.hpp
+++ include/oly/log.hpp
@@ -2,19 +2,19 @@
 
 #include <string>
 
-enum class severity {
-    CRITICAL, ERROR, WARNING, INFO, HINT, DEBUG, TRACE
-};
+enum class severity;
 
-enum class logopt : unsigned {
-    NONE       = 0,
-    WAIT       = 1 << 0, // Wait for user input
-    TO_STDERR  = 1 << 1, // Print to std::cerr
-    NO_PREFIX  = 1 << 2, // Don't print severity prefix
-    UPPERCASE  = 1 << 3  // Uppercase the message
-};
+inline bool operator<(severity a, severity b);
+inline bool operator>(severity a, severity b);
+inline bool operator<=(severity a, severity b);
+inline bool operator>=(severity a, severity b);
+
+enum class logopt : unsigned;
 
 inline logopt operator|(logopt a, logopt b);
 inline logopt& operator|=(logopt& a, logopt b);
 
-void Log(severity level, const std::string& message, logopt opts = logopt::NONE);
+namespace Log {
+inline severity log_level;
+void Log(severity level, const std::string& message, logopt opts = logopt::NONE, std::string cmd = "");
+}
