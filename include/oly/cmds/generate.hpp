83aafc7ea22651cc7ee6ee81edd75d078f180a84
anonymousgrasshopper <antoine.s.27182@gmail.com>
2025-08-27 11:36:43 +0200

    wip: implement generate command

--- include/oly/cmds/generate.hpp
+++ include/oly/cmds/generate.hpp
@@ -1,8 +1,16 @@
 #pragma once
 
 #include "oly/cmds/command.hpp"
+#include <filesystem>
 
 class Generate : public Command {
+private:
+	void generate(std::string source);
+
+	void create_latex_file(std::filesystem::path latex_file_path);
+	void create_pdf(std::filesystem::path latex_file_path);
+	void open_pdf_viewer(std::filesystem::path latex_file_path);
+
 public:
 	Generate();
 	int execute() override;
--- src/cmds/generate.cpp
+++ src/cmds/generate.cpp
@@ -1,7 +1,28 @@
+#include <filesystem>
+
 #include "oly/cmds/generate.hpp"
+#include "oly/config.hpp"
+#include "oly/utils.hpp"
+
+namespace fs = std::filesystem;
 
 Generate::Generate() {}
 
+void create_latex_file(std::filesystem::path latex_file_path) {}
+void create_pdf(std::filesystem::path latex_file_path) {}
+void open_pdf_viewer(std::filesystem::path latex_file_path) {}
+
+void Generate::generate(std::string source) {
+	config["source"] = source;
+	fs::path latex_file_path(expand_vars(config["output_directory"].as<std::string>()));
+	create_latex_file(latex_file_path);
+	create_pdf(latex_file_path);
+	open_pdf_viewer(latex_file_path);
+}
+
 int Generate::execute() {
+	for (std::string source : positional_args) {
+		generate(source);
+	}
 	return 0;
 }
