25442f435d2700d6f6e2861de0d8ce11adb1f135
anonymousgrasshopper <antoine.s.27182@gmail.com>
2025-12-22 13:45:12 +0100

    fix: load config file in search command

--- src/cmds/search.cpp
+++ src/cmds/search.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 
 #include "oly/cmds/search.hpp"
-#include "oly/log.hpp"
 #include "oly/utils.hpp"
 
 Search::Search() {}
 
 int Search::execute() {
+	load_config_file();
+
 	if (positional_args.empty()) {
 		for (const std::string& problem : utils::prompt_user_for_problems()) {
 			std::cout << problem << '\n';
